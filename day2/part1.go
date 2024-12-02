6e4116cbef11f9237b3194ae97bf270eff75a810
Nick Angeli <nicangeli@gmail.com>
2024-12-02 20:40:30 +0000

    rmeove breaks from part1

--- day2/part1.go
+++ day2/part1.go
@@ -1,66 +1,67 @@
 package main
 
 import (
-	_ "embed"
 	"math"
 	"strconv"
 	"strings"
 )
+
+func parseReport(report string) ([]int, error) {
+	var levels []int
+
+	for _, level := range strings.Split(report, " ") {
+		num, err := strconv.Atoi(level)
+		if err != nil {
+			return nil, err
+		}
+		levels = append(levels, num)
+	}
+
+	return levels, nil
+}
+
+func isReportSafe(levels []int) bool {
+	if len(levels) < 2 {
+		return true
+	}
+
+	isIncrementing := levels[0] < levels[1]
+	for i, current := range levels {
+		if i == len(levels)-1 {
+			return true
+		}
+
+		next := levels[i+1]
+		if isIncrementing && (next < current) {
+			return false
+		}
+
+		if !isIncrementing && (next > current) {
+			return false
+		}
+
+		diff := int(math.Abs(float64(next - current)))
+
+		if diff > 3 || diff < 1 {
+			return false
+		}
+
+	}
+
+	return true
+}
 
 func part1(file string) (int, error) {
 	safe := 0
 	lines := strings.Split(file, "\n")
 
 	for _, line := range lines {
-		levelsStr := strings.Split(line, " ")
-		levelsInt := make([]int, len(levelsStr))
-
-		for i, levelStr := range levelsStr {
-			num, err := strconv.Atoi(levelStr)
-			if err != nil {
-				return 0, err
-			}
-			levelsInt[i] = num
+		levels, err := parseReport(line)
+		if err != nil {
+			return 0, err
 		}
 
-		if len(levelsInt) < 2 {
-			safe += 1
-			break
-		}
-
-		isSafe := true
-		isIncrementing := levelsInt[0] < levelsInt[1]
-		for i, current := range levelsInt {
-			if (i + 1) == len(levelsInt) {
-				break
-			}
-
-			next := levelsInt[i+1]
-			if next == current {
-				isSafe = false
-				break
-			}
-
-			if isIncrementing && (next < current) {
-				isSafe = false
-				break
-			}
-
-			if !isIncrementing && (next > current) {
-				isSafe = false
-				break
-			}
-
-			diff := int(math.Abs(float64(next - current)))
-
-			if diff > 3 {
-				isSafe = false
-				break
-			}
-
-		}
-
-		if isSafe {
+		if isReportSafe(levels) {
 			safe += 1
 		}
 
